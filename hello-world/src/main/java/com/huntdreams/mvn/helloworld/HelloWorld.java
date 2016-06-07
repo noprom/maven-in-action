bc1d871c14466d93c7e4477ffcecb8bede17e3f2
noprom <tyee.noprom@qq.com>
2016-06-07 23:08:54 +0800

    update project pom

---
+++ hello-world/src/main/java/com/huntdreams/mvn/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.huntdreams.mvn.helloworld;
+
+/**
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 6/7/16 10:22 PM.
+ */
+public class HelloWorld {
+    public String sayHello() {
+        return "Hello Maven";
+    }
+
+    public static void main(String[] args) {
+        System.out.print(new HelloWorld().sayHello());
+    }
+}
