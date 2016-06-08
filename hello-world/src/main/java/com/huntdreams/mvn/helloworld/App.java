f7f6be40cef86e84d89ce89b134459abd132f1be
noprom <tyee.noprom@qq.com>
2016-06-08 23:51:10 +0800

    jar files

---
+++ hello-world/src/main/java/com/huntdreams/mvn/helloworld/App.java
@@ -0,0 +1,12 @@
+package com.huntdreams.mvn.helloworld;
+
+/**
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 6/7/16 10:22 PM.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.print("My first maven shade app");
+    }
+}
